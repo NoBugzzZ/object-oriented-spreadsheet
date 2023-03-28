4f9021adf829a4f9472a360cc2930f127a04119e
NoBugzzZ <739651338@qq.com>
2023-03-28 20:33:53 +0800

    report

--- src/craft/data.js
+++ src/craft/data.js
@@ -84,6 +84,165 @@ const AccountLayout = [
   ["${Account.netEarnings}"],
 ];
 
+const ReportSchemaSource = {
+  type: "object",
+  title: "Report",
+  properties: {
+    year: {
+      type: "array",
+      items: {
+        $ref: "#/$defs/Year",
+      }
+    }
+  },
+  $defs: {
+    Year: {
+      type: "object",
+      title: "Year",
+      properties: {
+        year: {
+          type: "string"
+        },
+        date: {
+          type: "array",
+          items: {
+            $ref: "#/$defs/Date"
+          }
+        }
+      }
+    },
+    Date: {
+      type: "object",
+      title: "Date",
+      properties: {
+        date: {
+          type: "string"
+        },
+        salesTotal: {
+          type: "number",
+          formula: "SUM(electricity,fuel,industry)"
+        },
+        electricityUsage: {
+          type: "number"
+        },
+        fuelUsage: {
+          type: "number"
+        },
+        industryUsage: {
+          type: "number"
+        },
+        city: {
+          $ref: "#/$defs/City"
+        },
+        industry: {
+          $ref: "#/defs/Industry"
+        }
+      }
+    },
+    City: {
+      type: "object",
+      title: "City",
+      properties: {
+        NanJing: {
+          type: "number"
+        },
+        WuXi: {
+          type: "number"
+        },
+        XuZhou: {
+          type: "number"
+        },
+        ChangZhou: {
+          type: "number"
+        },
+        SuZhou: {
+          type: "number"
+        },
+        NanTong: {
+          type: "number"
+        },
+        LianYunGang: {
+          type: "number"
+        },
+        HuaiAn: {
+          type: "number"
+        },
+        YanCheng: {
+          type: "number"
+        },
+        YangZhou: {
+          type: "number"
+        },
+        ZhenJiang: {
+          type: "number"
+        },
+        TaiZhou: {
+          type: "number"
+        },
+        SuQian: {
+          type: "number"
+        },
+      }
+    },
+    Industry: {
+      type: "object",
+      title: "Industry",
+      properties: {
+        JLSH: {
+          type: "number"
+        },
+        YZHX: {
+          type: "number"
+        },
+        YZSH: {
+          type: "number"
+        },
+        YZBSF: {
+          type: "number"
+        },
+        NJCHJ: {
+          type: "number"
+        },
+        NJHX: {
+          type: "number"
+        }
+      }
+    }
+  }
+}
+
+const ReportDataSource = null;
+
+const ReportLayout = [
+  [["${Report.Year}", "RIGHT", 1,
+    [
+      ["${Year.year}"],
+      ["日期", "销售总量", "发电", "城燃与工业燃料", "直供化工",
+        "南京", "无锡", "徐州", "常州", "苏州", "南通", "连云港", "淮安", "盐城", "扬州", "镇江", "泰州", "宿迁",
+        "金陵石化", "仪征化纤", "扬子石化", "扬子巴斯夫", "南京催化剂", "南京化学"
+      ]
+      [["${Year.Date}", "DOWN", 0,
+        [
+          [
+            "${Date.date}", "${Date.salesTotal}",
+            "${Date.electricityUsage}", "${Date.fuelUsage}", "${Date.industryUsage}",
+            "${Date.City.NanJing}", "${Date.City.WuXi}",
+            "${Date.City.XuZhou}", "${Date.City.ChangZhou}",
+            "${Date.City.SuZhou}", "${Date.City.NanTong}",
+            "${Date.City.LianYunGang}", "${Date.City.HuaiAn}",
+            "${Date.City.YanCheng}", "${Date.City.YangZhou}",
+            "${Date.City.ZhenJiang}", "${Date.City.TaiZhou}",
+            "${Date.City.SuQian}",
+            "${Date.Industry.JLSH}", "${Date.Industry.YZHX}",
+            "${Date.Industry.YZSH}", "${Date.Industry.YZBSF}",
+            "${Date.Industry.NJCHJ}", "${Date.Industry.NJHX}",
+          ]
+        ]
+      ]]
+    ],
+  ]],
+];
+
 export {
   AccountSchemaSource, AccountDataSource, AccountLayout,
 }
